bc4b2543a99f55b595528f48f1f5d70142fa63ca
Javier <javier@Javiers-MacBook-Air.local>
2026-06-02 14:01:00 -0300

    code review

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,4 +1,6 @@
-﻿List<Tarea> tareasPendiente = new List<Tarea>();
+﻿using System.Runtime.InteropServices;
+
+List<Tarea> tareasPendiente = new List<Tarea>();
 List<Tarea> tareasRealizada = new List<Tarea>();
 
 Random aux = new Random();
@@ -7,79 +9,104 @@ Tarea[] tareas = new Tarea[a];
 int c = 1000;
 string dato;
 int id;
-void buscarPorPalabra(string dato){
-    foreach (Tarea tarea in tareasPendiente){
-        if (dato.CompareTo(tarea.Descripcion) ==  0){
-            Console.WriteLine("------Tareas Pendientes------");
-            Console.WriteLine("Descripcion: "+tarea.Descripcion);
-            Console.WriteLine("id: "+tarea.TareaID);
-            Console.WriteLine("Duracion: "+tarea.Duracion);
-        }
-    }
-    
-    foreach (Tarea tarea in tareasRealizada){
-        if (dato.CompareTo(tarea.Descripcion) ==  0){
-            Console.WriteLine("------Tareas Realizada------");
-            Console.WriteLine("Descripcion: "+tarea.Descripcion);
-            Console.WriteLine("id: "+tarea.TareaID);
-            Console.WriteLine("Duracion: "+tarea.Duracion);
-        }
-    }
-}
 
-void mostrarLista(){
-    Console.WriteLine("------Tareas Pendientes------");
-    int s =1;
-    foreach (var tarea in tareasPendiente){
-        Console.WriteLine("Tarea N°"+s);
-        Console.WriteLine("Descripcion: "+tarea.Descripcion);
-        Console.WriteLine("id: "+tarea.TareaID);
-        Console.WriteLine("Duracion: "+tarea.Duracion);
-        s++;
-    }
-
-    s =1;
-
-    Console.WriteLine("------Tareas Realizadas------");
-    foreach (var tarea in tareasRealizada){
-        Console.WriteLine("Tarea N°"+s);
-        Console.WriteLine("Descripcion: "+tarea.Descripcion);
-        Console.WriteLine("id: "+tarea.TareaID);
-        Console.WriteLine("Duracion: "+tarea.Duracion);
-        s++;
-    }
-}
-
-
-for (int i = 0; i < a; i++){
+for (int i = 0; i < a; i++)
+{
     tareas[i] = new Tarea();
-    tareas[i].Descripcion = "Descripcion"+i;
+    tareas[i].Descripcion = "Descripcion" + i;
     tareas[i].TareaID = c;
     c++;
-    tareas[i].Duracion = aux.Next(10,100);
+    tareas[i].Duracion = aux.Next(10, 100);
 
     tareasPendiente.Add(tareas[i]);
 }
-Console.WriteLine("Cantidad de Tareas: "+(a+1));
+Console.WriteLine("Cantidad de Tareas: " + (a + 1));
 
 string x;
-do{
+do
+{
     Console.WriteLine("Ingrese una opcion: \n1)Mostrar listas \n2)Mover a tareas realizadas \n3)Buscar por descripcion");
     x = Console.ReadLine();
 
-    if (x == "1"){
-        mostrarLista();
-    }else if(x == "2"){
+    if (x == "1")
+    {
+        mostrarLista(tareasPendiente, "TAREAS PENDIENTES");
+        mostrarLista(tareasRealizada, "TAREAS REALIZADAS");
+    }
+    else if (x == "2")
+    {
         Console.WriteLine("Ingrese el id de la tarea pendiente:");
         dato = Console.ReadLine();
-        int.TryParse(dato, out id);
-        tareasRealizada.Add(tareasPendiente[id-1000]);
-        tareasPendiente.RemoveAt(id-1000);
-    }else if (x == "3"){
+        if (int.TryParse(dato, out id))
+        {
+            Tarea tareaBuscada = tareasPendiente.Where(p => p.TareaID == id).Single();
+            tareasPendiente.Remove(tareaBuscada);
+            tareasRealizada.Add(tareaBuscada);
+            //tareasRealizada.Add(tareasPendiente[id - 1000]);
+            //tareasPendiente.RemoveAt(id - 1000);
+        }
+    }
+    else if (x == "3")
+    {
         Console.WriteLine("Ingrese la descripcion de la tarea:");
         dato = Console.ReadLine();
-        buscarPorPalabra(dato);
+        Tarea tarea = buscarTareaPorPalabra(tareasPendiente, dato);
+
     }
     Console.WriteLine("Desea Continuar: \n1)No \n2)Si");
     x = Console.ReadLine();
 } while (x != "1");
+
+
+
+Tarea buscarTareaPorPalabra(List<Tarea> tareas, string dato)
+{
+    foreach (Tarea tarea in tareasPendiente)
+    {
+        if (dato.CompareTo(tarea.Descripcion) == 0)
+        {
+            return tarea;
+        }
+    }
+    return null;
+}
+
+
+void MostrarTarea(Tarea tarea)
+{
+    Console.WriteLine("Descripcion: "+tarea.Descripcion);
+    Console.WriteLine("id: "+tarea.TareaID);
+    Console.WriteLine("Duracion: "+tarea.Duracion);
+}
+
+// void buscarPorPalabra(string dato){
+//     foreach (Tarea tarea in tareasPendiente){
+//         if (dato.CompareTo(tarea.Descripcion) ==  0){
+//             Console.WriteLine("------Tareas Pendientes------");
+//             Console.WriteLine("Descripcion: "+tarea.Descripcion);
+//             Console.WriteLine("id: "+tarea.TareaID);
+//             Console.WriteLine("Duracion: "+tarea.Duracion);
+//         }
+//     }
+
+//     foreach (Tarea tarea in tareasRealizada){
+//         if (dato.CompareTo(tarea.Descripcion) ==  0){
+//             Console.WriteLine("------Tareas Realizada------");
+//             Console.WriteLine("Descripcion: "+tarea.Descripcion);
+//             Console.WriteLine("id: "+tarea.TareaID);
+//             Console.WriteLine("Duracion: "+tarea.Duracion);
+//         }
+//     }
+// }
+
+void mostrarLista(List<Tarea> tareas, string MensajeCabecera)
+{
+    Console.WriteLine(MensajeCabecera);
+    int s = 1;
+    foreach (var tarea in tareas)
+    {
+        Console.WriteLine("Tarea N°" + s);
+        MostrarTarea(tarea);
+        s++;
+    }
+}
